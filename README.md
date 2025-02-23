1516817b70bf22afaf32b5259a5ac327538c9708
debil03311 <andrei.hent@yahoo.com>
2025-02-23 23:14:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shinigami Scythe
+
+This extension removes content posted by people marked as transphobic by the [Shinigami Eyes] browser extension.
+
+Currently supported websites are:
+- Bluesky
+- Facebook
+- Twitter / X
+- YouTube
+
+[!NOTE]
+If you notice that blocked content is sneaking through, or if you would like for a website to be supported, open a PR or an issue and wait for an extension update.
---
+++ content.js
@@ -0,0 +1,96 @@
+class MutationReactor extends MutationObserver {
+  constructor(targetElement, callback, options={childList: true, subtree: true}) {
+    super(callback)
+
+    this.targetElement = targetElement
+    this.options = options
+  }
+
+  start() {
+    this.observe(this.targetElement, this.options)
+    return this
+  }
+  stop() {
+    this.disconnect()
+    return this
+  }
+  restart() {
+    this.stop()
+    this.start()
+  }
+  
+  runCallback() {
+    return this.callback?.()
+  }
+  
+  setOptions(options) {
+    const isObject = (options instanceof Object) && !(options instanceof Array)
+
+    if (!isObject) {
+      console.warn(`MutationReactor.setOptions: ${options} is not a valid Object, `
+                 + `continuing with ${JSON.stingify(this.options)}.`)
+      return this
+    }
+
+    this.options = options
+    this.restart()
+    return this
+  }
+
+  sneak(callback) {
+    this.stop()
+    const result = callback()
+    this.start()
+    return result
+  }
+}
+
+function makeSelector(...strings) {
+  return strings
+    .map((string) => string + ':has(.assigned-label-transphobic)')
+    .join(',')
+}
+
+// :has(.assigned-label-transphobic),
+const selectors = {
+  'x.com': makeSelector(
+    '[data-testid="tweet"]',
+    '[data-testid="UserCell"]',
+  ),
+
+  'youtube.com': makeSelector(
+    'ytd-video-renderer',
+    'ytd-channel-renderer',
+    'ytd-rich-item-renderer',
+    'ytd-reel-player-overlay-renderer',
+  ),
+
+  'bsky.app': makeSelector(
+    '[data-testid^="feedItem-by-"]',
+    '.css-175oi2r.r-1otgn73.r-1loqt21.r-1inuy60.r-1hfyk0a.r-ry3cjt.r-m611by',
+  ),
+
+  'facebook.com': makeSelector(
+    '.x1lliihq',
+    '[role="article"]',
+  ),
+}
+
+selectors['twitter.com'] = selectors['x.com']
+
+const reactor = new MutationReactor(document.body, () => {
+  try {
+    const posts = document.querySelectorAll(selectors[location.hostname])
+  }
+  catch (error) {
+    console.error(error)
+    alert(`Shinigami Scythe: Query selector error. Check the console for details.`)
+    reactor.stop()
+  }
+
+  if (!posts.length)
+    return
+
+  reactor.sneak(()=> posts.forEach((post) => post.remove()))
+})
+  .start()
